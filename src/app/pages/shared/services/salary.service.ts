b918fa162416262c74b2bdfdca0ab44c88e6c486
Felipe <fliptm95@gmail.com>
2019-08-22 20:04:26 -0300

    ärchive of configurations APIs

--- src/app/pages/shared/services/salary.service.ts
+++ src/app/pages/shared/services/salary.service.ts
@@ -2,15 +2,15 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Salary } from "../models/salary.model";
 import { Injectable } from "@angular/core";
+import { URL_API } from "../config/api";
 
 @Injectable()
 export class SalaryService {
-  private apiPath = "http://localhost:4567";
   constructor(private httpc: HttpClient) {}
 
   public sendSalaryTottality(salaryTotallity: string): Observable<Salary[]> {
     return this.httpc.get<Salary[]>(
-      `${this.apiPath}/salary?salary=${salaryTotallity}`
+      `${URL_API}/salary?salary=${salaryTotallity}`
     );
   }
 }
