1c822f2b604f3e4cf4a195ac77780a739fdc572f
Felipe <fliptm95@gmail.com>
2019-08-22 20:47:17 -0300

    Create button component shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,15 @@ import { HttpClientModule } from "@angular/common/http";
 import { SweetAlert2Module } from "@sweetalert2/ngx-sweetalert2";
 import { LoaderComponent } from "./pages/shared/components/loader/loader.component";
 import { HiringSchemeTableComponent } from "./pages/shared/components/hiring-scheme-table/hiring-scheme-table.component";
+import { ButtonComponent } from './pages/shared/components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoaderComponent,
-    HiringSchemeTableComponent
+    HiringSchemeTableComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
